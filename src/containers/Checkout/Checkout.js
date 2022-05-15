3a96ed48fb2564e6c57a94da9b50cbfc8e58f426
Areeba Rukhsar <areebarukhsar797@gmail.com>
2022-05-15 12:04:40 -0700

    checkout

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import ChecoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import ContactData from "./ContactData/ContactData";
+
+class Checkout extends Component {
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinueHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
+
+  render() {
+    let summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? (
+        <Redirect to="/" />
+      ) : null;
+
+      summary = (
+        <div>
+          {purchasedRedirect}
+          <ChecoutSummary
+            checkoutContinued={this.checkoutContinueHandler}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            ingredients={this.props.ings}
+          />
+          <Route
+            path={this.props.match.url + "/contact-data"}
+            component={ContactData}
+          />
+        </div>
+      );
+    }
+    return summary;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased,
+  };
+};
+
+export default connect(mapStateToProps)(Checkout);
+s
