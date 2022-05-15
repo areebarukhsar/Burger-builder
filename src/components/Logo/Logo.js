a5692e220525fbf6d9244d11abf1a2de23b34cc2
Areeba Rukhsar <areebarukhsar797@gmail.com>
2022-05-15 12:00:19 -0700

    logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,1 +1,13 @@
 
+import React from "react";
+import burgerLogo from "../../assets/images/burger-logo.png";
+import "./Logo.css";
+const logo = (props) => {
+  return (
+    <div className="Logo" style={{ height: props.height }}>
+      <img src={burgerLogo} alt="MyBurger" />
+    </div>
+  );
+};
+
+export default logo;
