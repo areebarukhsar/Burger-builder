bf40da1e9aaaec051ff0077fcccf51b0cd780a9b
Areeba Rukhsar <areebarukhsar797@gmail.com>
2022-05-15 12:01:25 -0700

    commit

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,1 +1,29 @@
 
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import "./SideDrawer.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxillary/Auxillary";
+
+const sideDrawer = (props) => {
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div
+        className={`SideDrawer ${props.open ? "Open" : "Close"} `}
+        onClick={props.closed}
+      >
+        <div className="logoMargin">
+          <Logo height="70px" />
+        </div>
+
+        <nav>
+          <NavigationItems isAuthenticated={props.isAuth} />
+        </nav>
+      </div>
+    </Aux>
+  );
+};
+
+export default sideDrawer;
