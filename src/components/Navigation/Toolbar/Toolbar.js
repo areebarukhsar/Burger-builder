7d8c4062adf83698df6492b052d9bcff80bd2f9b
Areeba Rukhsar <areebarukhsar797@gmail.com>
2022-05-15 12:02:33 -0700

    toolbr

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,1 +1,20 @@
 
+import React from "react";
+import Logo from "../../Logo/Logo";
+import "./Toolbar.css";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrowerToggle from "../SideDrawer/DrawerToggler/DrowerToggle";
+
+const toolbar = (props) => {
+  return (
+    <header className="Toolbar">
+      <DrowerToggle clicked={props.drowerToggleClicked} />
+      <Logo height="50px" />
+      <nav className="DesktopOnly">
+        <NavigationItems isAuthenticated={props.isAuth} />
+      </nav>
+    </header>
+  );
+};
+
+export default toolbar;
